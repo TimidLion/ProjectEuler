ab3c53c8c8571a9de41b6471b782ea4090709e19
TimidLion <TimidLion@naver.com>
2015-07-28 23:32:42 +0900

    solved problem7

--- problem7.cpp
+++ problem7.cpp
@@ -1,13 +1,81 @@
 #include<iostream>
-#include<cmath>
+#include<stdio.h>
+#include<string.h>
 using namespace std;
+class cheh
+{
+	private:
+			long* prime;
+			long size;
+	public:
+		cheh(int num)
+		{
+			this->size = num;
+			prime = new long [num+1];
+			prime[0] = 0;
+			prime[1] = 0;
+			for(long i = 2; i < size; i ++)
+				prime[i] = 1;
+		}
+		~cheh()
+		{
+			delete[] prime;
+		}
+		void getSize()
+		{
+			cout << "Size of the cheh is : " << this->size << endl;
+			return;
+		}
+		void docheh()
+		{
+			cout << "Doing cheh" << endl;
+			for(long i = 2; (i*i) <= this->size ; i++)
+			{
+				for(long j = i*i; j <= this->size ; j+=i)
+					prime[j] = 0;
+			}
+			return;
+		}
+		int getThPrime(int th = 0)
+		{
+			int counter = 0;
+			bool flag = false;
+			long manil;
+			for(long i = 2; i < this->size; i++)
+			{
+				if(prime[i] == 1)
+					counter++;
+				if(counter == th)
+				{
+					manil = i;
+					flag = true;
+					break;
+				}
+			}
+			if(flag)
+				return manil;
+			else
+				return 0;
+		}
+};
 
 int main()
 {	
 	int th = 0;
-	while(th != 10001)
+	int num = 10001;
+	bool manilthprime = false;
+	int size = 100000;
+	while(!manilthprime)
 	{
-		
-		th++;
+		cheh thousand(size);
+		thousand.getSize();
+		thousand.docheh();
+		long ok = thousand.getThPrime(num);
+		if(ok)
+		{
+			cout << ok << endl;
+			manilthprime = true;
+		}
+		size *= 10;
 	}
 }
